fd1cdbdd04be043721f0bbb1f0362b72a9d95200
MartiPuigGinebro <martipuig1996@ginebro.cat>
2021-02-05 18:09:32 +0100

    PuigMarti_Act_01_UsuPri_MySQL

---
+++ PuigMarti_Act_01_UsuPri_MySQL_Apartat_008.sql
@@ -0,0 +1,5 @@
+FLUSH PRIVILEGES;
+--------------
+--  FLUSH PRIVILEGES
+--------------
+-- Query OK, 0 rows affected (0.00 sec)
---
+++ PuigMarti_Act_01_UsuPri_MySQL_Apartat_013.sql
@@ -0,0 +1,5 @@
+mysql -h localhost -u root -password=1234 -v;
+DROP USER 'PuigMarti'@'localhost';
+--------------
+-- DROP USER 'PuigMarti'@'localhost'
+--------------

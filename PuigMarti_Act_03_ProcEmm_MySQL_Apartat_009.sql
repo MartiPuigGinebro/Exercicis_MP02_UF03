90fb0a9e33bda05c2a504b0fba8abc8acadcefa4
MartiPuigGinebro <martipuig1996@ginebro.cat>
2021-04-08 19:15:06 +0200

    PuigMarti_Act_03_ProcEmm_MySQL_Apartat_011.sql

---
+++ PuigMarti_Act_03_ProcEmm_MySQL_Apartat_009.sql
@@ -0,0 +1,41 @@
+use videoclub;
+drop procedure if exists act9;
+delimiter //
+create procedure act9()
+begin
+   declare recaptat bigint default 0;
+   declare pressu bigint default 0;
+   declare titol varchar(40);
+   declare rendibilitat varchar(15);
+   declare final int default false;
+   
+   declare elcursor cursor for
+      select titol_peli, recaudacio_peli, pressupost_peli
+      from PELLICULES;
+
+   declare continue handler for not found set final = 1;
+   open elcursor;
+   elbucle:loop
+      fetch elcursor into titol, recaptat, pressu;
+      
+      if final = 1 then
+         leave elbucle;
+      end if;
+      
+      if (pressu>recaptat) then
+         set rendibilitat = "Deficitari";
+      elseif (pressu*2 > recaptat) then
+         set rendibilitat = "Suficient";
+      else
+         set rendibilitat = "Bona";
+      end if;
+      
+      select titol, rendibilitat;
+   
+   end loop elbucle; 
+   close elcursor;
+end//
+
+delimiter ;
+
+call act9();
